1949d5d6df411c242b8e642730b5bd344b57fa35
KGMalith <it18258622@my.sliit.lk>
2022-05-16 11:58:12 +0530

    updated routes files

--- routes.js
+++ routes.js
@@ -5,5 +5,8 @@ const proxy = require('express-http-proxy');
 //Define Routing paths
 router.use('/auth', proxy('http://localhost:4001'));
 router.use('/wishlist', proxy('http://localhost:4002'));
+router.use('/cart', proxy('http://localhost:4003'));
+router.use('/product', proxy('http://localhost:4004'));
+router.use('/order', proxy('http://localhost:4005'));
 
 module.exports = router;
